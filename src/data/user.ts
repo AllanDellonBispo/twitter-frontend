972249145dadb93601ca278214550c159514606f
Allan Bispo <allandellon.desbra@gmail.com>
2025-02-14 17:19:10 -0300

    Final version the project twitter-frontend

---
+++ src/data/user.ts
@@ -0,0 +1,11 @@
+import { User } from "@/types/user";
+
+export const user: User = {
+    slug: 'ADB25',
+    name: 'Allan Bispo',
+    avatar: 'https://ui-avatars.com/api/?background=random&name=Allan&size=100',
+    cover: 'https://img.freepik.com/fotos-gratis/conceito-de-viagem-completo-com-pontos-de-referencia_23-2149153258.jpg',
+    bio: 'Desenvolvedor há mais de 6 anos.',
+    link: 'https://github.com/AllanDellonBispo',
+    postCount: 127
+}
